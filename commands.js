7055033db573dd099d782abb172a562cf45ddc1b
Isuki <40719262+isukioffi@users.noreply.github.com>
2020-03-07 16:26:53 +0100

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,9 @@
+var commands ={
+    'yo': function() {alert ('Bonjour Isuki!');},
+    'comment vas-tu': function() {alert ('Très bien et toi?');},
+    'ceci est un très bon début pour le code': function() {alert ('Effectivement');}
+    }
+    
+    
+
+ 
